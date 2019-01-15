38e8e37454e409322ab0e6209f139dd398f12e34
RaphaelTo <paiva.raphaelt@gmail.com>
2019-01-16 00:09:28 +0100

    Add video object

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,16 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\VideoRepository;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(VideoRepository $videoRepository)
     {
+        $video = $videoRepository->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'video' => $video,
         ]);
     }
 }
