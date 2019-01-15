89c16b022dc9778db2672f6d89bdfc93aaa85b9b
RaphaelTo <paiva.raphaelt@gmail.com>
2019-01-16 00:40:12 +0100

    Add method addUser

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,18 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegisterController extends AbstractController
 {
     /**
      * @Route("/register", name="register")
      */
-    public function index()
+    public function addUser(Request $request,UserPasswordEncoderInterface $passwordEncoder)
     {
+        $user = new User();
+        $form= $this->createForm(RegisterUserType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $password= $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $entityManager=$this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('home');
+        }
+
         return $this->render('register/index.html.twig', [
-            'controller_name' => 'RegisterController',
+            'form' => $form->createView(),
         ]);
     }
 }
