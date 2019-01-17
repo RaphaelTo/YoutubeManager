27f506f7843341b2dcd9a28bd7abb72116ef9177
RaphaelTo <paiva.raphaelt@gmail.com>
2019-01-18 00:37:38 +0100

    Add method login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\LoginUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Entity\User;
 use App\Form\RegisterUserType;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
@@ -33,5 +35,19 @@ class SecurityController extends AbstractController
         return $this->render('register/index.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils){
+        $user = new User();
+        $form=$this->createForm(LoginUserType::class, $user);
+
+        return $this->render('security/login.html.twig', [
+            'error'=> $authenticationUtils->getLastAuthenticationError(),
+            'form'=> $form->createView()
+        ]);
+
     }
 }
