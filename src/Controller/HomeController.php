7dc2fe06faa0892853af3b60dd4fde581f5b76fe
RaphaelTo <paiva.raphaelt@gmail.com>
2019-01-19 00:40:24 +0100

    Add detailvideo function

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\VideoRepository;
@@ -18,6 +19,16 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'video' => $video,
+        ]);
+    }
+
+    /**
+     * @Route("/video/{id}", name="detailVideo")
+     */
+    public function videoDetail(int $id, VideoRepository $videoRepository){
+        $video = $videoRepository->find($id);
+        return $this->render('home/detailVideo.html.twig', [
+            'video'=>$video
         ]);
     }
 }
